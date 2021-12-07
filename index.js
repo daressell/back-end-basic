f35f6b1a49ddb09ca490bc453bec2022d37392c2
daressell <olegcasovskoj@gmail.com>
2021-12-07 13:57:50 +0300

    add controller collector

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { json, urlencoded } from "express"
 import dotenv from "dotenv"
 import router from "./routes/router.js"
 import cors from "cors"
+import fs from "fs"
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
@@ -11,7 +12,13 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
-app.use("/", router)
+fs.readdirSync("./routes").map((file) => {
+  import(`./routes/${file}`).then((module) => {
+    app.use("/", module.default)
+  })
+})
+
+// app.use("/", router)
 
 app.listen(PORT, () => {
   console.log("start server")
