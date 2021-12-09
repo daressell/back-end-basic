196478d42ecdd8a7d64b81867ca0bb6ecff226ae
daressel <nevaliashkaoxxxy@gmail.com>
2021-12-09 22:29:41 +0300

    fix includes for middleware

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,14 +1,10 @@
 const jwt = require("jsonwebtoken")
 
 module.exports = (req, res, next) => {
-  if (req.url.includes("registration", "login")) return next()
+  if (req.url.includes("registration") || req.url.includes("login")) return next()
   const token = req.headers.authorization
-  if (!token)
-    return res.status(403).send("A token is required for authentication")
+  if (!token) return res.status(403).send("A token is required for authentication")
 
-  req.userId = jwt.verify(
-    req.headers.authorization,
-    process.env.TOKEN_KEY
-  ).userId
+  req.userId = jwt.verify(req.headers.authorization, process.env.TOKEN_KEY).userId
   return next()
 }
